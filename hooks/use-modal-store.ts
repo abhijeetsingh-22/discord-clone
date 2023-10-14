747467d3a327064d5260e36bc30a64149ac0c8fe
Abhijeet Singh <asingh9829@gmail.com>
2023-10-14 14:13:27 +0530

    Added functionality to update server

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import {create} from "zustand"
 
-export type ModalType ="createSErver" | "invite"
+export type ModalType ="createSErver" | "invite" | "editServer"
 
 interface ModalData{
   server?:Server
