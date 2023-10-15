b10149bc2ed212b559d2174e13c8d6d8c7ad3381
Abhijeet Singh <asingh9829@gmail.com>
2023-10-15 18:59:28 +0530

    Added create channel functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -61,6 +61,7 @@ const ServerHeader = ({server, role}: ServerHeaderProps) => {
         {isModerator && (
           <DropdownMenuItem
             className="px-3 py-2 text-sm cursor-pointer "
+            onClick={()=>onOpen("createChannel",{server})}
           >
             Create Channel
             <PlusCircle className=" h-4 w-4 ml-auto"/>
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import {create} from "zustand"
 
-export type ModalType ="createSErver" | "invite" | "editServer"| "members"
+export type ModalType ="createSErver" | "invite" | "editServer"| "members" | "createChannel"
 
 interface ModalData{
   server?:Server
