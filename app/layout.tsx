324283f8a7872c2e2fa7c904d228033ebb724e13
Abhijeet Singh <asingh9829@gmail.com>
2023-11-19 01:04:01 +0530

    added the socket io

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {ClerkProvider} from '@clerk/nextjs'
 import {cn} from '@/lib/utils'
 import {ThemeProvider} from '@/components/providers/theme-provider'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { SocketProvider } from '@/components/providers/socket-provider'
 const font = Open_Sans({subsets: ['latin']})
 
 export const metadata: Metadata = {
@@ -20,16 +21,19 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 				lang='en'
 				suppressHydrationWarning
 			>
+				<body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
 				<ThemeProvider
 					attribute='class'
 					defaultTheme='dark'
 					enableSystem={false}
 					storageKey='discord-theme'
 				>
-					<body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
+					<SocketProvider>
             <ModalProvider/>
-            {children}</body>
+            {children}
+					</SocketProvider>
 				</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	)
